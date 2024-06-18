1a45fa67a1bf5acee4c0eca851a32fe573e671c9
Masumi Kanai <masumi.net@gmail.com>
2024-06-18 10:01:39 +0900

    feat: imple user app

--- run.sh
+++ run.sh
@@ -3,9 +3,16 @@
 
 QEMU=qemu-system-riscv32
 
+OBJCOPY=llvm-objcopy
 CC=clang
 CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib"
 
+# shell
+$CC $CFLAGS -Wl,-Tuser.ld -Wl,-Map=shell.map -o shell.elf shell.c user.c common.c
+$OBJCOPY --set-section-flags .bss=alloc,contents -O binary shell.elf shell.bin
+$OBJCOPY -Ibinary -Oelf32-littleriscv shell.bin shell.bin.o
+
+# kernel
 $CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
     kernel.c common.c
 
