2050900e5f01df374aef522e5e674bfcbd91b60a
Masumi Kanai <masumi.net@gmail.com>
2024-06-20 09:18:31 +0900

    feat: implement read/write disk from kernel

--- kernel.c
+++ kernel.c
@@ -69,6 +69,115 @@ long getchar(void) {
   return ret.error;
 }
 
+uint32_t virtio_reg_read32(unsigned offset) {
+  return *((volatile uint32_t *)(VIRTIO_BLK_PADDR + offset));
+}
+
+uint64_t virtio_reg_read64(unsigned offset) {
+  return *((volatile uint64_t *)(VIRTIO_BLK_PADDR + offset));
+}
+
+void virtio_reg_write32(unsigned offset, uint32_t value) {
+  *((volatile uint32_t *)(VIRTIO_BLK_PADDR + offset)) = value;
+}
+
+void virtio_reg_fetch_and_or32(unsigned offset, uint32_t value) {
+  virtio_reg_write32(offset, virtio_reg_read32(offset) | value);
+}
+
+struct virtio_virtq *blk_request_vq;
+struct virtio_blk_req *blk_req;
+paddr_t blk_req_paddr;
+unsigned blk_capacity;
+
+struct virtio_virtq *virtq_init(unsigned index) {
+  paddr_t virtq_paddr =
+      alloc_pages(align_up(sizeof(struct virtio_virtq), PAGE_SIZE) / PAGE_SIZE);
+  struct virtio_virtq *vq = (struct virtio_virtq *)virtq_paddr;
+  vq->queue_index = index;
+  vq->used_index = (volatile uint16_t *)&vq->used.index;
+  virtio_reg_write32(VIRTIO_REG_QUEUE_SEL, index);
+  virtio_reg_write32(VIRTIO_REG_QUEUE_NUM, VIRTQ_ENTRY_NUM);
+  virtio_reg_write32(VIRTIO_REG_QUEUE_ALIGN, 0);
+  virtio_reg_write32(VIRTIO_REG_QUEUE_PFN, virtq_paddr);
+
+  return vq;
+}
+
+void virtq_kick(struct virtio_virtq *vq, int desc_index) {
+  vq->avail.ring[vq->avail.index % VIRTQ_ENTRY_NUM] = desc_index;
+  vq->avail.index++;
+  __sync_synchronize();
+  virtio_reg_write32(VIRTIO_REG_QUEUE_NOTIFY, vq->queue_index);
+  vq->last_used_index++;
+}
+
+bool virtq_is_busy(struct virtio_virtq *vq) {
+  return vq->last_used_index != *vq->used_index;
+}
+
+void read_write_disk(void *buf, unsigned sector, int is_write) {
+  if (sector >= blk_capacity / SECTOR_SIZE) {
+    printf("virtio: tried to read/write sector=%d, but capacity is %d\n",
+           sector, blk_capacity / SECTOR_SIZE);
+    return;
+  }
+  blk_req->sector = sector;
+  blk_req->type = is_write ? VIRTIO_BLK_T_OUT : VIRTIO_BLK_T_IN;
+  if (is_write)
+    memcpy(blk_req->data, buf, SECTOR_SIZE);
+
+  struct virtio_virtq *vq = blk_request_vq;
+  vq->descs[0].addr = blk_req_paddr;
+  vq->descs[0].len = sizeof(uint32_t) * 2 + sizeof(uint64_t);
+  vq->descs[0].flags = VIRTQ_DESC_F_NEXT;
+  vq->descs[0].next = 1;
+
+  vq->descs[1].addr = blk_req_paddr + offsetof(struct virtio_blk_req, data);
+  vq->descs[1].len = SECTOR_SIZE;
+  vq->descs[1].flags = VIRTQ_DESC_F_NEXT | (is_write ? 0 : VIRTQ_DESC_F_WRITE);
+  vq->descs[1].next = 2;
+
+  vq->descs[2].addr = blk_req_paddr + offsetof(struct virtio_blk_req, status);
+  vq->descs[2].len = sizeof(uint8_t);
+  vq->descs[2].flags = VIRTQ_DESC_F_WRITE;
+
+  virtq_kick(vq, 0);
+  while (virtq_is_busy(vq))
+    ;
+  if (blk_req->status != 0) {
+    printf("virtio: wan: failed to read/write sector=%d status=%d\n", sector,
+           blk_req->status);
+    return;
+  }
+  if (!is_write)
+    memcpy(buf, blk_req->data, SECTOR_SIZE);
+}
+
+void virtio_blk_init(void) {
+  if (virtio_reg_read32(VIRTIO_REG_MAGIC) != 0x74726976) {
+    PANIC("virtio: invalid magic value");
+  }
+  if (virtio_reg_read32(VIRTIO_REG_VERSION) != 1)
+    PANIC("virtio: invalid version");
+  if (virtio_reg_read32(VIRTIO_REG_DEVICE_ID) != VIRTIO_DEVICE_BLK)
+    PANIC("virtio: invalid device id");
+
+  virtio_reg_write32(VIRTIO_REG_DEVICE_STATUS, 0);
+  virtio_reg_fetch_and_or32(VIRTIO_REG_DEVICE_STATUS, VIRTIO_STATUS_ACK);
+  virtio_reg_fetch_and_or32(VIRTIO_REG_DEVICE_STATUS, VIRTIO_STATUS_DRIVER);
+  virtio_reg_fetch_and_or32(VIRTIO_REG_DEVICE_STATUS, VIRTIO_STATUS_FEAT_OK);
+  blk_request_vq = virtq_init(0);
+  virtio_reg_write32(VIRTIO_REG_DEVICE_STATUS, VIRTIO_STATUS_DRIVER_OK);
+
+  blk_capacity = virtio_reg_read64(VIRTIO_REG_DEVICE_CONFIG + 0) * SECTOR_SIZE;
+  printf("virtio-blk: capcity is %d bytes\n", blk_capacity);
+
+  blk_req_paddr =
+      alloc_pages(align_up(sizeof(*blk_req), PAGE_SIZE) / PAGE_SIZE);
+  blk_req = (struct virtio_blk_req *)blk_req_paddr;
+}
+
 __attribute__((naked)) __attribute__((aligned(4))) void kernel_entrty(void) {
   __asm__ __volatile__("csrrw sp, sscratch, sp\n"
                        "addi sp, sp, -4*31\n"
@@ -258,6 +367,7 @@ struct process *create_process(const void *image, uint32_t image_size) {
        paddr += PAGE_SIZE) {
     map_page(page_table, paddr, paddr, PAGE_R | PAGE_W | PAGE_X);
   }
+  map_page(page_table, VIRTIO_BLK_PADDR, VIRTIO_BLK_PADDR, PAGE_R | PAGE_W);
 
   for (uint32_t off = 0; off < image_size; off += PAGE_SIZE) {
     paddr_t page = alloc_pages(1);
@@ -347,6 +457,14 @@ void kernel_main(void) {
   memset(__bss, 0, (size_t)__bss_end - (size_t)__bss);
   printf("\n\nHello %s in kernel\n", "World!");
   WRITE_CSR(stvec, (uint32_t)kernel_entrty);
+  virtio_blk_init();
+
+  char buf[SECTOR_SIZE];
+  read_write_disk(buf, 0, false);
+  printf("first sector: %s\n", buf);
+  strcpy(buf, "hello from kernel!!\n");
+  read_write_disk(buf, 0, true);
+
   // __asm__ __volatile__("unimp"); // 無効な命令
   //
   // paddr_t paddr0 = alloc_pages(2);
